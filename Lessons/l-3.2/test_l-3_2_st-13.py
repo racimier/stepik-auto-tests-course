bec2ee8686d1f990c0fc78dace50edb3fd731745
Egl <eglath@mail.ru>
2021-02-12 09:51:18 +0300

    test_l-3_2_st-13.py was refactored

--- Lessons/l-3.2/test_l-3_2_st-13.py
+++ Lessons/l-3.2/test_l-3_2_st-13.py
@@ -9,7 +9,7 @@ class TestForm(unittest.TestCase):
         
     def form(self, link):
         browser = self.driver
-        browser.implicitly_wait(5)
+        browser.implicitly_wait(2)
         browser.get(link)
 
         # Ваш код, который заполняет обязательные поля
@@ -20,23 +20,18 @@ class TestForm(unittest.TestCase):
         # Отправляем заполненную форму
         browser.find_element_by_css_selector("button.btn").click()
         # Проверяем, что смогли зарегистрироваться
-        
-        # находим элемент, содержащий текст
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-        welcome_text = browser.find_element_by_tag_name('h1').text
-        return welcome_text
+
+        return browser.find_element_by_tag_name('h1').text
         
     def test_form1(self):
         link = 'http://suninjuly.github.io/registration1.html'
-        registration_result = self.form(link)
-
-        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+        self.assertEqual("Congratulations! You have successfully registered!",
+                         self.form(link))
 
     def test_form2(self):
         link = 'http://suninjuly.github.io/registration2.html'
-        registration_result = self.form(link)
-
-        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+        self.assertEqual("Congratulations! You have successfully registered!",
+                         self.form(link))
 
     def tearDown(self):  # обязательное название метода
         self.driver.close()
