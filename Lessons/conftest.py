f914c98c0fcb99da029358eaad1020b88539273b
Egl <eglath@mail.ru>
2021-02-19 12:25:59 +0300

    add test_rerun.py to l-3.6 and update conftest.py

--- Lessons/conftest.py
+++ Lessons/conftest.py
@@ -10,13 +10,24 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
+        from selenium.webdriver.chrome.options import Options
+
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(firefox_binary='c:/from_C/Program Files/Mozilla Firefox/firefox.exe')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_binary='c:/from_C/Program Files/Mozilla Firefox/firefox.exe',
+                                    firefox_profile=fp)
+
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
